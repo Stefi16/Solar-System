5bb64d580a8eafb4e39af9148933302b5bbf8d04
Stefka <67965028+Stefi16@users.noreply.github.com>
2021-05-31 08:26:17 +0300

    Add files via upload

---
+++ Shapes/Ellipse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+
+namespace Solar_System
+{
+    [Serializable]
+    public class Ellipse
+    {
+        public Color MyColor { get; set; }
+        public Point Location { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Ellipse()
+        {
+
+        }
+        public Ellipse(Point pointy, int width, int height)
+        {
+            this.Location = new Point(pointy.X, pointy.Y);
+            this.Width = width;
+            this.Height = height;
+        }
+        public void Paint(Graphics graphy)
+        {
+            using (Pen pen = new Pen(MyColor, 4))
+            {
+                graphy.DrawEllipse(pen, this.Location.X, this.Location.Y, this.Width, this.Height);
+            }
+        }
+        public bool ContainsEllipse(Point pointy)
+        {
+
+            Point center = new Point(
+                  this.Location.X + this.Width / 2,
+                  this.Location.Y + this.Height / 2);
+
+            double radiusX = this.Width / 2;
+            double radiusY = this.Height / 2;
+            var point = new Point(pointy.X - center.X, pointy.Y - center.Y);
+            double result = ((point.X * point.X) / (radiusX * radiusX)) + ((point.Y * point.Y) / (radiusY * radiusY));
+
+            if (result >= 0.95 && result <= 1.029)
+                return true;
+
+            return false;
+
+        }
+    }
+}
---
+++ Shapes/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+
+namespace Solar_System
+{
+    [Serializable]
+    public class Rectangle
+    {
+        public Point Position { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public Color Color { get; set; }
+        public Point OriginalPosition { get; set; }
+        public int TopYLeft => Position.Y;
+        public int TopXLeft => Position.X;
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(Color color, Point point, int width, int height)
+        {
+            this.Color = color;
+            this.Position = point;
+            this.Width = width;
+            this.Height = height;
+
+        }
+        public void PaintRectangle(Graphics graphics)
+        {
+            using (SolidBrush brushy = new SolidBrush(
+                Color.FromArgb(
+                Math.Min(byte.MaxValue, Color.R + 54),
+                Math.Min(byte.MaxValue, Color.G + 80),
+                Math.Min(byte.MaxValue, Color.B + 66))))
+            {
+                graphics.FillRectangle(brushy, Position.X, Position.Y, Width, Height);
+            }
+            using (Pen pen = new Pen(Color, 4))
+            {
+                graphics.DrawRectangle(pen, Position.X, Position.Y, Width, Height);
+            }
+        }
+
+        public bool Contains(Point point)
+        {
+            return Position.X < point.X && point.X < Position.X + Width &&
+                Position.Y < point.Y && point.Y < Position.Y + Height;
+        }
+    }
+}
